f441ad00f2cfccd409b8cf740c16e46a9a9e9f37
lemon <540846207@qq.com>
2018-03-11 23:27:49 +0800

    feat: 查询翻页

--- dao/blog.js
+++ dao/blog.js
@@ -2,16 +2,22 @@ const sql = require('./mysql-config');
 
 
 module.exports = {
-    lists: function(ctx){
-        const body = ctx.request.body;
-        console.log('1',body)
-        let page = (body.page - 1)|| 0
-        const start = page * body.num;
-        const end = parseInt(body.num);
+    lists: async function(ctx){
+        const params = ctx.request.body;
+        console.log('1',params)
+        let page = (params.page - 1)|| 0
+        const start = page * params.pageSize;
+        const end = parseInt(params.pageSize);
+        let total = 0;
+        
+        await sql.query('select count(*) from essay').then(function(result) {
+            total = result[0]['count(*)'];
+         });
+
         const res = sql.query('select * from essay Order By gmt_created Desc limit ?,?;',[start, end]).then(function(result) {
-            console.log('result',result)
             return {
                 data: result,
+                total: total,
                 code: 1
             };
         }, function(error){
@@ -20,6 +26,7 @@ module.exports = {
                 status: false
             };
         });
+        
         return res
     },
     write: function(ctx){
